b7cc2dba910be474d29841dab83ca4c4d5f45db1
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-26 13:31:11 +0300

    guess game stage 1

--- 05/starter/script.js
+++ 05/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+// document.querySelector('.message').textContent = '🎉 Correct answer!';
+let score = 0;
+let highScore = 0;
+
+let sectrtNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.score').textContent = score;
+console.log(sectrtNumber);
+// document.querySelector('.number').textContent = sectrtNumber;
+// document.querySelector('.guess').value = 10;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  let userInput = Number(document.querySelector('.guess').value);
+  if (!userInput)
+    document.querySelector('.message').textContent = 'No number entered!';
+  else if (userInput !== sectrtNumber) {
+    score -= 1;
+    document.querySelector('.score').textContent = score;
+  } else {
+    score += 1;
+    document.querySelector('.message').textContent = 'correct Asnwer 🎖';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = sectrtNumber;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+  document.querySelector('.guess').value = '';
+});
