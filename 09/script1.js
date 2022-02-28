b1cc2dc2e3f16d0ed407bbf6d864c5a76c975ba6
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-28 21:28:48 +0300

    challange #1 soccer done

--- 09/script1.js
+++ 09/script1.js
@@ -60,4 +60,26 @@ const game = {
     x: 3.25,
     team2: 6.5,
   },
+  printGoals: function (...args) {
+    for (let i = 0; i < args.length; i++) {
+      console.log(args[i]);
+    }
+  },
 };
+
+// const players1 = game.players[0];
+// const players2 = game.players[1];
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = players1;
+const [...allPlayers] = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+const { team1, x: draw, team2 } = game.odds;
+const {
+  odds: { team1: team11, x: drwa1, team2: team22 },
+} = game;
+
+game.printGoals('player1', 'player2');
+console.log(team11, drwa1, team22);
+
+team1 < team2 && console.log('team 1 is more likely to win');
+team1 > team2 && console.log('team 2 is more likely to win');
