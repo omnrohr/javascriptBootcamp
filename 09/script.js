e6dd2b1ec08ec2e51692657c2a8462a6b4d9144b
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-02-28 20:09:06 +0300

    challange #1 soccer betting

--- 09/script.js
+++ 09/script.js
@@ -6,12 +6,14 @@ const flights =
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
+  rName: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
+  order: function (starterMenuIndex, mainMenuIndex) {
+    return [this.starterMenu[starterMenuIndex], this.mainMenu[mainMenuIndex]];
+  },
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +28,82 @@ const restaurant = {
       close: 24,
     },
   },
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+//////// destructuring arrays
+/*
+let arr = [1, 2, 3]; //not effected
+let [x, y, z] = arr; // create a copy of the array
+const [a, b, c] = [1, 2, 3];
+console.log(a, b, c);
+
+x = 5;
+console.log(arr, x);
+console.log([x, y, z]);
+console.log(x, y, z);
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+//changing id effecting the main list.
+first = 'new first item';
+console.log(first, restaurant.categories[0]);
+
+let [firstEl, , thrirdEl] = restaurant.categories; // leave a space to get the next element.
+console.log(first, thrirdEl);
+
+[thrirdEl, firstEl] = [firstEl, thrirdEl]; // reassinging values
+console.log(firstEl, thrirdEl);
+//reverse the result order
+let [theSecondItem, theFirstItem] = restaurant.order(0, 0);
+console.log(theSecondItem, theFirstItem);
+
+//default unstructring
+
+const [dd = 0, bb = 0, cc = 0] = [1];
+console.log(dd, bb, cc);
+*/
+
+/////////destructuring objects
+/*
+let { rName, openingHours, mainMenu } = restaurant;
+console.log(rName, openingHours, mainMenu);
+
+let {
+  rName: newName,
+  openingHours: newOpiningHours,
+  mainMenu: newMainMenu,
+} = restaurant;
+console.log(newName, newOpiningHours, newMainMenu);
+
+let {
+  rName: restaurantName,
+  mainMenu: restaurantMenu,
+  tags: restaurantTags = ['item 1', 'item 2'],
+} = restaurant;
+console.log(restaurantName, restaurantMenu, restaurantTags);
+
+let a = 23;
+let b = 222;
+
+const item = { a: 10, b: 12, c: 14 };
+console.log(a, b);
+const abObj = { a, b };
+({ a, b } = item); //mutating varaibles.
+console.log(a, b, abObj);
+*/
+const { openingHours } = restaurant;
+const { fri } = openingHours;
+console.log(fri, { openingHours });
+
+// Destructuring Objects
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
