1dd01a402c4a003b4500b61a1cf7d903a461a3e4
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-03-02 15:30:48 +0300

    challange 4 section9 done

--- 09/challang4/script.js
+++ 09/challang4/script.js
@@ -2,9 +2,12 @@
 // Coding Challenge #4
 
 /* 
-Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+Write a program that receives a list of variable names 
+written in underscore_case and convert them to camelCase.
 
-The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+The input will come from a textarea inserted into the DOM 
+(see code below), and conversion will happen when the 
+button is pressed.
 
 THIS TEST DATA (pasted to textarea)
 underscore_case
@@ -20,15 +23,36 @@ someVariable        ✅✅✅
 calculateAge        ✅✅✅✅
 delayedDeparture    ✅✅✅✅✅
 
-HINT 1: Remember which character defines a new line in the textarea 😉
-HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
-HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
-HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+HINT 1: Remember which character defines a new line in
+ the textarea 😉
+HINT 2: The solution only needs to work for a
+ variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. 
+Tackle that only after you have the variable name 
+conversion working 😉
+HINT 4: This challenge is difficult on purpose, 
+so start watching the solution in case you're stuck.
+ Then pause and continue!
 
 Afterwards, test with your own test data!
 
 GOOD LUCK 😀
 */
 
+const convertToCamelCase = function (str) {
+  const splitedStr = str.split('\n');
+  for (const i of splitedStr) {
+    const [first, second] = i.trim().toLowerCase().split('_');
+    const cammelCase =
+      (first + second[0].toUpperCase() + second.slice(1)).padEnd(20) + ';);)';
+    console.log(cammelCase);
+  }
+};
+
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const textarea1 = document.querySelector('textarea').value;
+  console.log(convertToCamelCase(textarea1));
+});
