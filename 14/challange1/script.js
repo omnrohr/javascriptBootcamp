24eb13a873a9ed327ec3a6d6b417f3ce29289b91
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-03-24 21:56:19 +0300

    section 14 challange 1 done

--- 14/challange1/script.js
+++ 14/challange1/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// create class of car
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerator = function () {
+  return (this.speed += 10);
+};
+
+Car.prototype.break = function () {
+  return (this.speed -= 5);
+};
+
+const BMW = new Car('BMW', 120);
+const mercedes = new Car('mercedes', 95);
+console.log('BMW accelerator', BMW.accelerator());
+console.log('mercedes accelerator', mercedes.accelerator());
+console.log('BMW break', BMW.break());
+console.log('mercedes break', mercedes.break());
