705914cdd4f24b4c2f359ce41a86a4c1ec1c203b
omnrohr <67183475+omnrohr@users.noreply.github.com>
2022-03-27 13:10:18 +0300

    section 14 challange 2 done

--- 14/challange1/script.js
+++ 14/challange1/script.js
@@ -20,3 +20,51 @@ console.log('BMW accelerator', BMW.accelerator());
 console.log('mercedes accelerator', mercedes.accelerator());
 console.log('BMW break', BMW.break());
 console.log('mercedes break', mercedes.break());
+
+class CarClass {
+  constructor(maker, speed) {
+    this.make = maker;
+    this.speed = speed;
+  }
+  accelerator = function () {
+    return (this.speed += 10);
+  };
+
+  break = function () {
+    return (this.speed -= 5);
+  };
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const BMW2 = new CarClass('BMW', 120);
+const mercedes2 = new CarClass('mercedes', 95);
+console.log('BMW2 accelerator', BMW2.accelerator());
+console.log('mercedes2 accelerator', mercedes2.accelerator());
+console.log('BMW2 break', BMW2.break());
+console.log('mercedes2 break', mercedes2.break());
+console.log('bmw2 speed us', mercedes2.speedUS);
+BMW2.speedUS = 80;
+console.log('mercedes2 set speedUS', BMW2.speedUS, BMW2.speed);
+const car3 = Object.create(Car);
+car3.__proto__ = Car.prototype;
+// Car.init = function (maker, speed) {
+//   this.maker = maker;
+//   this.speed = speed;
+// };
+// car3.init('Toyota', 100);
+
+car3.speed = 100;
+car3.maker = 'toyota';
+console.log('after adding init method to Car Class: ', car3.accelerator());
+// console.log(
+//   'read methods from type inheritance: ',
+//   car3.accelerator(),
+//   car3.break()
+// );
+
+console.log(car3.__proto__, Car.prototype);
